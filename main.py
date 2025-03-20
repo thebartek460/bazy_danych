3b7cf439be4411cdb0cb4379056b52aee12a3ffa
Gołębiowski Maciej <student05@student8.fire.uxxu.eu>
2025-03-20 10:40:10 +0100

    jakies tam kolejne zmiany

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def hello():
-	print("hello world")
+	for i in range(6):
+		print("hello world")
+		print(i)
+
+
+def cos():
+	for i in range(1, 8):
+		print("*" * i)
 
 if __name__ == "__main__":
 	hello()
+	cos()
